fbc0d00013d959a3d3cf256dd62ab95d88b33ddb
Bart Pelgrim <bartpelgrim@hotmail.com>
2021-02-28 17:49:32 +0100

    Replace NaN with None

--- app/dataset.py
+++ app/dataset.py
@@ -1,20 +1,22 @@
 from io import BytesIO
 from typing import Dict
 
-from pandas import DataFrame
+import pandas
 from xarray import open_dataset
 
 
-def file_content_to_dataframe(file_content: bytes) -> DataFrame:
+def file_content_to_dataframe(file_content: bytes) -> pandas.DataFrame:
     with BytesIO(file_content) as input_file:
         with open_dataset(input_file) as dataset:
             df = dataset.to_dataframe()
             return df
 
 
-def obs_to_dict(df: DataFrame) -> Dict[str, list]:
+def obs_to_dict(df: pandas.DataFrame) -> Dict[str, list]:
+    # replace NaN with None
+    df_with_nones = df.where(pandas.notnull(df), None)
     station_list = []
-    for row in df.itertuples():
+    for row in df_with_nones.itertuples():
         station = dict(
             name=row.stationname,
             lat=row.lat,
