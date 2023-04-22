8a068819b6922bd13bc889115494af5024ac615b
Bart Pelgrim <bartpelgrim@hotmail.com>
2023-04-22 20:34:56 +0200

    Add Paper to sidebar

--- ui/src/components/Sidebar/Sidebar.js
+++ ui/src/components/Sidebar/Sidebar.js
@@ -1,4 +1,5 @@
 import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
 import ElementMenu from "../SelectionMenu/ElementMenu";
 import { elementConfiguration } from "../../model/Elements";
 import TimeButtonGroup from "../TimeButtonGrid/TimeButtonGroup";
@@ -9,36 +10,38 @@ export default function Sidebar(props) {
   const { selectedElement, setSelectedElement, dateTime, back10Minutes, forward10Minutes, getLatestObs } = props;
 
   return (
-    <Grid
-      container
-      direction={"column"}
-      justify={"center"}
-      alignItems={"center"}
-      display={"flex"}
-    >
-      <Grid item xs={3} sx={{fontSize: '2vh', margin: 3}}>
-        Controls
+    <Paper elevation={2} sx={{padding: 2, margin: 2}}>
+      <Grid
+        container
+        direction={"column"}
+        justify={"center"}
+        alignItems={"center"}
+        display={"flex"}
+      >
+        <Grid item xs={3} sx={{fontSize: '2vh', margin: 3}}>
+          Controls
+        </Grid>
+        <Grid item xs={3}>
+          Selected element:
+        </Grid>
+        <Grid item xs={3}>
+          <ElementMenu
+            selectedElement={selectedElement}
+            setSelectedElement={setSelectedElement}
+            elementConfiguration={elementConfiguration}
+          />
+        </Grid>
+        <Grid item xs={3} paddingTop={2}>
+          {dateTime?.toLocaleTimeString([], TimeOptions) ?? "No data available"}
+        </Grid>
+        <Grid item xs={3}>
+          <TimeButtonGroup
+            backButtonAction={back10Minutes}
+            forwardButtonAction={forward10Minutes}
+            latestButtonAction={getLatestObs}
+          />
+        </Grid>
       </Grid>
-      <Grid item xs={3}>
-        Selected element:
-      </Grid>
-      <Grid item xs={3}>
-        <ElementMenu
-          selectedElement={selectedElement}
-          setSelectedElement={setSelectedElement}
-          elementConfiguration={elementConfiguration}
-        />
-      </Grid>
-      <Grid item xs={3} paddingTop={2}>
-        {dateTime?.toLocaleTimeString([], TimeOptions) ?? "No data available"}
-      </Grid>
-      <Grid item xs={3}>
-        <TimeButtonGroup
-          backButtonAction={back10Minutes}
-          forwardButtonAction={forward10Minutes}
-          latestButtonAction={getLatestObs}
-        />
-      </Grid>
-    </Grid>
+    </Paper>
   );
 }
