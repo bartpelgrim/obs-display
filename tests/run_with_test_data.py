64f73a093014bf5641fc9f0a996e2d71f941811a
Bart Pelgrim <bartpelgrim@hotmail.com>
2024-08-02 21:12:19 +0200

    Make sure mock observations are not cleaned up

--- tests/run_with_test_data.py
+++ tests/run_with_test_data.py
@@ -8,5 +8,6 @@ from tests.helpers.mock_data import mock_data
 if __name__ == '__main__':
     with mock_data() as mock_dataset:
         with mock.patch('app.knmi_obs.KnmiApi.get_latest_obs', return_value=mock_dataset):
-            writer = ObservationWriter()
-            writer.run()
+            with mock.patch('app.dataset.OBSERVATION_TTL_DAYS', 9999):
+                writer = ObservationWriter()
+                writer.run()
