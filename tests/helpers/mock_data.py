11945db839d8d8ee49e79cab3d30a1cc99a9957b
Bart Pelgrim <bartpelgrim@hotmail.com>
2026-01-12 21:42:43 +0100

    Fix relative path in test data

--- tests/helpers/mock_data.py
+++ tests/helpers/mock_data.py
@@ -1,8 +1,12 @@
 from contextlib import contextmanager
+import os
+
+from app.config import APP_PATH
 
 
 @contextmanager
 def mock_data():
-    with open('tests/data/KMDS__OPER_P___10M_OBS_L2_202103060830.nc', 'rb') as test_file:
+    data_path = os.path.join(APP_PATH, '..', 'tests/data/KMDS__OPER_P___10M_OBS_L2_202103060830.nc')
+    with open(data_path, 'rb') as test_file:
         test_dataset = test_file.read()
         yield test_dataset
